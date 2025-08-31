acd19523ff21467693f142e6f0458f8e2f2a50cc
Carlo D'Ambrosio <carlo@ql4b.net>
2025-08-31 20:59:49 +0200

    feat: handle throttled response

--- main.tf
+++ main.tf
@@ -147,16 +147,16 @@ resource "aws_api_gateway_gateway_response" "throttled" {
   }
 }
 
-resource "aws_api_gateway_gateway_response" "default_5xx" {
-  for_each      = toset(local.stages)
-  rest_api_id   = aws_api_gateway_rest_api.api[each.key].id
-  response_type = "DEFAULT_5XX"
-  status_code   = "502"
+# resource "aws_api_gateway_gateway_response" "default_5xx" {
+#   for_each      = toset(local.stages)
+#   rest_api_id   = aws_api_gateway_rest_api.api[each.key].id
+#   response_type = "DEFAULT_5XX"
+#   status_code   = "502"
   
-  response_templates = {
-    "application/json" = jsonencode({
-      error   = "Service Unavailable"
-      message = "Downstream service temporarily unavailable"
-    })
-  }
-}
+#   response_templates = {
+#     "application/json" = jsonencode({
+#       error   = "Service Unavailable"
+#       message = "Downstream service temporarily unavailable"
+#     })
+#   }
+# }
